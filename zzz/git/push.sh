d393a8b2bd6dfcc1b8e9ce4a5f2de0975a9ba372
kuangyufei <kuangyufei@126.com>
2020-10-23 00:09:17 +0800

    鸿蒙源码分析系列篇 https://blog.csdn.net/kuangyufei                 https://my.oschina.net/u/3751245

--- zzz/git/push.sh
+++ zzz/git/push.sh
@@ -4,7 +4,9 @@ git commit -m  '鸿蒙源码分析系列篇 https://blog.csdn.net/kuangyufei
 git push origin
 git push gitee_origin master
 git push github_origin master
+git push coding_origin master
 
 #git remote add github_origin git@github.com:kuangyufei/kernel_liteos_a_note.git 
 #git remote add gitee_origin git@gitee.com:weharmony/kernel_liteos_a_note.git
 #git remote add origin git@codechina.csdn.net:kuangyufei/kernel_liteos_a_note.git
+#git remote add coding_origin git@e.coding.net:weharmony/harmony/kernel_liteos_a_note.git
