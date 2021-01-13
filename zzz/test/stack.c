c1826ac4f6965974bde8eba8793ac3162441fe84
kuangyufei <kuangyufei@126.com>
2021-01-13 13:13:51 +0800

    栈是构建底层运行的基础,鸿蒙用栈方式递减满栈(FD)     搜索 @note_pic 可查看绘制的全部字符图     搜索 @note_why 是尚未看明白的地方，有看明白的，请Pull Request完善     搜索 @note_thinking 是一些的思考和建议     搜索 @note_#if0 是由第三方项目提供不在内核源码中定义的极为重要结构体，为方便理解而添加的。     搜索 @note_good 是给源码点赞的地方

--- zzz/test/stack.c
+++ zzz/test/stack.c
@@ -1,14 +1,20 @@
 //armv7-a clang (trunk)
 #include <stdio.h>
+#include <math.h>
+
+int square(int a,int b){
+    return a*b;
+}
+
 int fp(int b)
 {
     int a = 1;
-    return a+b;
+    return square(a+b,a+b);
 }
 
 int main()
 {
-    int sum = 0;
+    int sum = 1;
     for(int a = 0;a < 100; a++){
         sum = sum + fp(a);
     }
