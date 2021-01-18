5f9835cde3b311387e6428ea0a2f5f5dfd6a2c1b
kuangyufei <kuangyufei@126.com>
2021-01-18 09:24:50 +0800

    将markdown部分移到https://gitee.com/weharmony/docs     搜索 @note_pic 可查看绘制的全部字符图     搜索 @note_why 是尚未看明白的地方，有看明白的，请Pull Request完善     搜索 @note_thinking 是一些的思考和建议     搜索 @note_#if0 是由第三方项目提供不在内核源码中定义的极为重要结构体，为方便理解而添加的。     搜索 @note_good 是给源码点赞的地方     https://weharmony.github.io/     公众号: 鸿蒙内核源码分析

--- zzz/git/push.sh
+++ zzz/git/push.sh
@@ -1,5 +1,5 @@
 git add -A
-git commit -m  '整理技术文章内容
+git commit -m  '将markdown部分移到https://gitee.com/weharmony/docs
     搜索 @note_pic 可查看绘制的全部字符图
     搜索 @note_why 是尚未看明白的地方，有看明白的，请Pull Request完善
     搜索 @note_thinking 是一些的思考和建议
