727889e8ced54314ab0fb32806d845a27ba631a4
kuangyufei <kuangyufei@126.com>
2021-06-11 17:05:28 +0800

    注解 NuttX 文件系统     百万汉字注解 + 百篇博客分析 => 挖透鸿蒙内核源码     国内:https://weharmony.gitee.io     国外:https://weharmony.github.io

---
+++ zzz/git/nuttx_pull.sh
@@ -0,0 +1,7 @@
+cd E:\harmony\third_party\third_party_NuttX
+#git fetch
+git pull origin master
+
+#git pull --rebase origin master
+
+cd -
---
+++ zzz/git/nuttx_push.sh
@@ -0,0 +1,20 @@
+cd E:\harmony\third_party\third_party_NuttX
+
+git add -A
+git commit -m  ' 注解 NuttX 文件系统
+    百万汉字注解 + 百篇博客分析 => 挖透鸿蒙内核源码
+    国内:https://weharmony.gitee.io
+    国外:https://weharmony.github.io
+'
+
+git push  origin master
+#git push  gitee_origin master
+#git push  github_origin master
+#git push  coding_origin master
+
+#git remote add github_origin git@github.com:kuangyufei/kernel_liteos_a_note.git 
+#git remote add gitee_origin git@gitee.com:weharmony/kernel_liteos_a_note.git
+#git remote add origin git@codechina.csdn.net:kuangyufei/kernel_liteos_a_note.git
+#git remote add coding_origin git@e.coding.net:weharmony/harmony/kernel_liteos_a_note.git
+
+cd -
